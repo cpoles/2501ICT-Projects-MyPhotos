b2d09d9db78d4eeaa7d7003edbb59e9107e3500c
Carlos Poles <carlos.poles@griffithuni.edu.au>
2016-04-24 12:52:36 +1000

    Added class to be tested inside the test Class

--- MyPhotosTests/PhotoTests.swift
+++ MyPhotosTests/PhotoTests.swift
@@ -10,7 +10,23 @@ import XCTest
 
 class PhotoTests: XCTestCase {
     
-    
+    class Photo {
+        // MARK - Properties
+        
+        var title: String?
+        var url: NSURL
+        var tags: [String]?
+        var imageData: NSData?
+        
+        // initialisation
+        
+        init(title: String? = nil, url: NSURL, tags: [String]? = nil) {
+            self.title = title
+            self.url = url
+            self.tags = tags
+        }
+        
+    }
     
     
     
