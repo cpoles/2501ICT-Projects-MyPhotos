792585bbd49f9059df8185ed1b0081ba30407648
Carlos Poles <carlos.poles@griffithuni.edu.au>
2016-04-24 14:25:47 +1000

    Added test Class for the PhotoCollection class.

---
+++ MyPhotosTests/PhotoCollectionTests.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoCollectionTests.swift
+//  MyPhotos
+//
+//  Created by Carlos Poles on 24/04/2016.
+//  Copyright © 2016 Carlos Poles. All rights reserved.
+//
+
+import XCTest
+
+class PhotoCollectionTests: XCTestCase {
+    
+    
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+
+}
