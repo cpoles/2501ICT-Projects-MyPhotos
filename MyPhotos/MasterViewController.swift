7251e66908b3ce5e2b072353ea55eddf0bec2c6e
Carlos Poles <carlos.poles@griffithuni.edu.au>
2016-04-25 00:13:51 +1000

    added array that contains photo elements

--- MyPhotos/MasterViewController.swift
+++ MyPhotos/MasterViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class MasterViewController: UICollectionViewController {
     
-    var photos = PhotoCollection()
+    var photoColletion = [Photo]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        photoColletion.append(Photo(title: "QUT", url: NSURL(string: "https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Queensland_University_of_Technology_(logo).svg/1017px-Queensland_University_of_Technology_(logo).svg.png")!, tags: ["qut", "queensland", "university"]))
+        photoColletion.append(Photo(title: "Griffith University", url: NSURL(string: "https://upload.wikimedia.org/wikipedia/en/2/2a/Griffith_University_logo.png")!, tags: ["griffith", "queensland", "university"]))
+        photoColletion.append(Photo(title: "ACU", url: NSURL(string: "http://www.pccevents.com.au/wp-content/uploads/2011/03/logo-acu-small.jpg")!, tags: ["catholic", "queensland", "university"]))
+        
     }
 
     override func didReceiveMemoryWarning() {
