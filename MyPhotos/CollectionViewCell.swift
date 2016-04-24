25a1f2ac77e29f4ddada23904008ed7b613fd7ea
Carlos Poles <carlos.poles@griffithuni.edu.au>
2016-04-24 22:08:21 +1000

    Added CollectionViewCell controller

---
+++ MyPhotos/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  MyPhotos
+//
+//  Created by Carlos Poles on 24/04/2016.
+//  Copyright © 2016 Carlos Poles. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
